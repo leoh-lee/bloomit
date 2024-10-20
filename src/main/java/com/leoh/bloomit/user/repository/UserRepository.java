e89c563abf3f1710669c21c77c1dedb495290265
nimoh <spakers38@gmail.com>
2024-10-21 01:47:45 +0900

    feat: Add UserRepository Jpa interface

---
+++ src/main/java/com/leoh/bloomit/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.leoh.bloomit.user.repository;
+
+import com.leoh.bloomit.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
