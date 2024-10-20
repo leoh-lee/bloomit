93f7db7453ed24345400aaf82ba4d20f26c7a429
nimoh <spakers38@gmail.com>
2024-10-21 01:58:07 +0900

    feat: Add JwtUtil

---
+++ src/main/java/com/leoh/bloomit/security/jwt/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.leoh.bloomit.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${jwt.secret-key}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return getPayload(token).get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return getPayload(token).get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return getPayload(token).getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredTime) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredTime))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    private Claims getPayload(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+    }
+
+}
