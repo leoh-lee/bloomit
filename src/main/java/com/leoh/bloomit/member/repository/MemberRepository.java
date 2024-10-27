ebd7140642a931aa1632e079073aa2ff75e80963
nimoh <spakers38@gmail.com>
2024-10-27 23:58:55 +0900

    feat: JWT 기본 기능 구축

---
+++ src/main/java/com/leoh/bloomit/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.leoh.bloomit.member.repository;
+
+import com.leoh.bloomit.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
---
+++ src/main/java/com/leoh/bloomit/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,122 @@
+package com.leoh.bloomit.security.jwt;
+
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String GRANT_TYPE = "Bearer";
+    private final SecretKey secretKey;
+
+    @Value("${jwt.access-token-expiration-time}")
+    private long accessTokenExpirationTime;
+
+    @Value("${jwt.refresh-token-expiration-time}")
+    private long refreshTokenExpirationTime;
+
+    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+    public JwtToken generateToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+
+        String accessToken = Jwts.builder()
+                .subject(authentication.getName())
+                .claim("auth", authorities)
+                .expiration(new Date(now + accessTokenExpirationTime))
+                .signWith(secretKey)
+                .compact();
+
+        String refreshToken = Jwts.builder()
+                .expiration(new Date(now + refreshTokenExpirationTime))
+                .signWith(secretKey)
+                .compact();
+
+        return JwtToken.builder()
+                .grantType(GRANT_TYPE)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+
+        if (claims.get("auth") == null) {
+            throw new IllegalArgumentException("권한 정보가 없는 토큰입니다.");
+        }
+
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String headerValue = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(headerValue) && headerValue.startsWith(GRANT_TYPE)) {
+            return headerValue.substring(7);
+        }
+        return null;
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+}
