3ca1f47415f98775fcb799a7b43e1778006a31f2
nimoh <spakers38@gmail.com>
2024-10-28 22:38:25 +0900

    feat: SignUpDto 유효성 검증 추가

--- src/main/java/com/leoh/bloomit/auth/dto/SignUpDto.java
+++ src/main/java/com/leoh/bloomit/auth/dto/SignUpDto.java
@@ -1,12 +1,30 @@
 package com.leoh.bloomit.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class SignUpDto {
+
+    @NotBlank
+    @Size(min = 5, max = 20)
     private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 8~20자의 영어, 숫자, 특수기호(@$!%*?&) 중 하나 이상을 포함하여야 합니다.")
     private String password;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 16)
     private String nickname;
 }
