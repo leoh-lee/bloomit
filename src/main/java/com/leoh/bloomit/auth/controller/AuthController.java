b0c55986029a5633b548881def296c9d74c71fb1
nimoh <spakers38@gmail.com>
2024-10-28 21:59:21 +0900

    feat: 로그인 시 SignIn DTO Valid 추가

--- src/main/java/com/leoh/bloomit/auth/controller/AuthController.java
+++ src/main/java/com/leoh/bloomit/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.leoh.bloomit.auth.controller;
 
 import com.leoh.bloomit.auth.service.AuthService;
-import com.leoh.bloomit.member.dto.SignInDto;
+import com.leoh.bloomit.auth.dto.SignInDto;
 import com.leoh.bloomit.security.jwt.JwtToken;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +22,11 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public JwtToken signIn(@RequestBody SignInDto signInDto) {
+    public JwtToken signIn(@Valid @RequestBody SignInDto signInDto) {
         JwtToken jwtToken = authService.signIn(signInDto);
+        if (ObjectUtils.isEmpty(jwtToken)) {
+            throw new UsernameNotFoundException("User not found");
+        }
         log.info("jwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
 
         return jwtToken;
