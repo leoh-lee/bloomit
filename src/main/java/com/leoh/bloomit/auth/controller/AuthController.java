b7d5606d444bb1657224c016d061ee8d1f9aa51a
nimoh <spakers38@gmail.com>
2024-10-28 22:39:17 +0900

    feat: sign-up controller 추가

--- src/main/java/com/leoh/bloomit/auth/controller/AuthController.java
+++ src/main/java/com/leoh/bloomit/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.leoh.bloomit.auth.controller;
 
+import com.leoh.bloomit.auth.dto.SignUpDto;
 import com.leoh.bloomit.auth.service.AuthService;
 import com.leoh.bloomit.auth.dto.SignInDto;
 import com.leoh.bloomit.security.jwt.JwtToken;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,14 +24,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public JwtToken signIn(@Valid @RequestBody SignInDto signInDto) {
+    public ResponseEntity<JwtToken> signIn(@Valid @RequestBody SignInDto signInDto) {
         JwtToken jwtToken = authService.signIn(signInDto);
         if (ObjectUtils.isEmpty(jwtToken)) {
             throw new UsernameNotFoundException("User not found");
         }
         log.info("jwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
 
-        return jwtToken;
+        return ResponseEntity.ok(jwtToken);
     }
 
+    @PostMapping("/sign-up")
+    public void signUp(@Valid @RequestBody SignUpDto signUpDto) {
+        authService.signUp(signUpDto);
+    }
 }
