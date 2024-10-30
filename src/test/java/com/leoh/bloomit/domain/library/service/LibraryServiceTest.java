74ef35c10b2d72d994580e8b8017c78405f7b2d1
homin.lee <homin.lee@rapeech.com>
2024-10-30 16:57:09 +0900

    test: 회원 ID로 서재 조회 서비스 레이어 테스트 코드 Mocking으로 변경
    - 기존에는 Classist의 관점에서 실제 데이터를 세팅하여 통합테스트를 하려고 했음.
    - LibraryRepositoryTest와 로직이 아예 동일해짐.
    - 중복 테스트를 제거하기 위해 Mocking 도입

--- src/test/java/com/leoh/bloomit/domain/library/service/LibraryServiceTest.java
+++ src/test/java/com/leoh/bloomit/domain/library/service/LibraryServiceTest.java
@@ -1,26 +1,22 @@
 package com.leoh.bloomit.domain.library.service;
 
 import com.leoh.bloomit.domain.book.entity.Book;
-import com.leoh.bloomit.domain.book.repository.BookRepository;
 import com.leoh.bloomit.domain.library.dto.response.LibrarySearchResponse;
 import com.leoh.bloomit.domain.library.entity.Library;
 import com.leoh.bloomit.domain.library.repository.LibraryRepository;
-import com.leoh.bloomit.domain.librarybook.entity.LibraryBook;
-import com.leoh.bloomit.domain.librarybook.repository.LibraryBookRepository;
 import com.leoh.bloomit.domain.member.entity.Member;
-import com.leoh.bloomit.domain.member.repository.MemberRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -29,35 +25,15 @@ class LibraryServiceTest {
     @Autowired
     private LibraryService libraryService;
 
-    @Autowired
+    @MockBean
     private LibraryRepository libraryRepository;
-
-    @Autowired
-    private LibraryBookRepository libraryBookRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @AfterEach
-    void tearDown() {
-        libraryBookRepository.deleteAllInBatch();
-        libraryRepository.deleteAllInBatch();
-        bookRepository.deleteAllInBatch();
-        memberRepository.deleteAllInBatch();
-    }
 
     @Test
     @DisplayName("특정 회원의 서재를 조회한다.")
     void findByMember() {
         //given
         Member member = createMember("username", "nickname", "name", "password");
-        memberRepository.save(member);
-
         Library library = Library.create(member);
-        libraryRepository.save(library);
 
         String bookTitle1 = "effective java";
         String bookTitle2 = "함께자라기";
@@ -67,28 +43,16 @@ class LibraryServiceTest {
         String author2 = "test";
         String author3 = "test";
 
-        Book effectiveJava = createBook(bookTitle1, author1);
-        Book growthTogether = createBook(bookTitle2, author2);
-        Book programmer = createBook(bookTitle3, author3);
-
-        bookRepository.saveAll(List.of(
-                effectiveJava,
-                growthTogether,
-                programmer
+        library.addBooks(List.of(
+                Book.builder().title(bookTitle1).author(author1).build(),
+                Book.builder().title(bookTitle2).author(author2).build(),
+                Book.builder().title(bookTitle3).author(author3).build()
         ));
 
-        LibraryBook libraryBook1 = LibraryBook.create(library, effectiveJava);
-        LibraryBook libraryBook2 = LibraryBook.create(library, growthTogether);
-        LibraryBook libraryBook3 = LibraryBook.create(library, programmer);
-
-        libraryBookRepository.saveAll(List.of(
-                libraryBook1,
-                libraryBook2,
-                libraryBook3
-        ));
+        when(libraryRepository.findByMemberId(anyLong())).thenReturn(library);
 
         //when
-        LibrarySearchResponse findLibrary = libraryService.findByMemberId(member.getId());
+        LibrarySearchResponse findLibrary = libraryService.findByMemberId(1L);
 
         //then
         assertThat(findLibrary.getBooks())
@@ -98,16 +62,6 @@ class LibraryServiceTest {
                         tuple(bookTitle2, author2),
                         tuple(bookTitle3, author3)
                 );
-    }
-
-    private Book createBook(String bookTitle, String author) {
-        return Book.builder()
-                .isbn(UUID.randomUUID().toString())
-                .title(bookTitle)
-                .author(author)
-                .publisher("인사이트")
-                .publicationDate(LocalDateTime.now())
-                .build();
     }
 
     private Member createMember(String username, String nickname, String name, String password) {
