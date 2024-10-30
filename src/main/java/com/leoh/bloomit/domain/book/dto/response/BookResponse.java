b7ac49b31e1fcf30345daa2547d2241847857bdb
homin.lee <homin.lee@rapeech.com>
2024-10-30 15:10:54 +0900

    feat: Book Reponsitory, BookResponse 추가

---
+++ src/main/java/com/leoh/bloomit/domain/book/dto/response/BookResponse.java
@@ -0,0 +1,46 @@
+package com.leoh.bloomit.domain.book.dto.response;
+
+import com.leoh.bloomit.domain.book.entity.Book;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BookResponse {
+
+    private Long id;
+
+    private String isbn;
+
+    private String title;
+
+    private String author;
+
+    private String publisher;
+
+    private LocalDateTime publicationDate;
+
+    private String imageUrl;
+
+    private String story;
+
+    private String description;
+
+    public static BookResponse fromEntity(Book book) {
+        return new BookResponse(
+                book.getId(),
+                book.getIsbn(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getPublisher(),
+                book.getPublicationDate(),
+                book.getImageUrl(),
+                book.getStory(),
+                book.getDescription()
+        );
+    }
+
+}
