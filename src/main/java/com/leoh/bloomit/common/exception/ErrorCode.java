1f3b832356471a21db5acda8bc52861a66f31f32
nimoh <spakers38@gmail.com>
2024-11-03 21:29:52 +0900

    refactor: MemberService#findByUsername에서 조회 결과가 없는 경우 ResourceNotFoundException로 예외 변경

--- src/main/java/com/leoh/bloomit/common/exception/ErrorCode.java
+++ src/main/java/com/leoh/bloomit/common/exception/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
 
     // ResourceNotFoundException
     GENRE_NOT_FOUND("Genre not found"),
+    MEMBER_NOT_FOUND("Member not found"),
     ;
 
     private final String message;
--- src/main/java/com/leoh/bloomit/domain/member/service/MemberService.java
+++ src/main/java/com/leoh/bloomit/domain/member/service/MemberService.java
@@ -1,11 +1,13 @@
 package com.leoh.bloomit.domain.member.service;
 
+import com.leoh.bloomit.common.exception.ResourceNotFoundException;
 import com.leoh.bloomit.domain.member.entity.Member;
 import com.leoh.bloomit.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.leoh.bloomit.common.exception.ErrorCode.MEMBER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +27,7 @@ public class MemberService {
 
     public Member findByUsername(String username) {
         return memberRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username dose not exist"));
+                .orElseThrow(() -> new ResourceNotFoundException(MEMBER_NOT_FOUND));
     }
 
 }
