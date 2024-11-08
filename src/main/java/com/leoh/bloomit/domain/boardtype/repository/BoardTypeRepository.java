a677d708fbe0e24480236b3ecaf097f1284ba905
nimoh <spakers38@gmail.com>
2024-11-08 22:26:43 +0900

    feat: BoardType 엔티티 제

--- src/main/java/com/leoh/bloomit/domain/boardtype/repository/BoardTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.leoh.bloomit.domain.boardtype.repository;
-
-import com.leoh.bloomit.domain.boardtype.entity.BoardType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardTypeRepository extends JpaRepository<BoardType, Long> {
-    BoardType findByName(String name);
-}
