4acc481741453f9d7cfba90382ef499054058a04
nimoh <spakers38@gmail.com>
2024-11-10 23:05:15 +0900

    refactor: MemberResponseDTO를 record로 변경

--- src/main/java/com/leoh/bloomit/domain/member/dto/response/MemberResponse.java
+++ src/main/java/com/leoh/bloomit/domain/member/dto/response/MemberResponse.java
@@ -1,28 +1,10 @@
 package com.leoh.bloomit.domain.member.dto.response;
 
 import com.leoh.bloomit.domain.member.entity.Member;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class MemberResponse {
-
-    private Long id;
-
-    private String username;
-
-    private String name;
-
-    private String nickname;
+public record MemberResponse(Long id, String username, String name, String nickname) {
 
     public static MemberResponse fromEntity(Member member) {
-        return new MemberResponse(
-                member.getId(),
-                member.getUsername(),
-                member.getName(),
-                member.getNickname()
-        );
+        return new MemberResponse(member.getId(), member.getUsername(), member.getName(), member.getNickname());
     }
 }
